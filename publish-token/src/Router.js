58d1f06e38d755479380fdc55d5409d71ad1ac2b
Chen <rachel_chen0915@outlook.com>
2021-09-03 12:49:48 +0800

    merged change and remove unused files

--- publish-token/src/Router.js
+++ publish-token/src/Router.js
@@ -3,9 +3,7 @@ import {HashRouter, Route, Switch} from 'react-router-dom';
 
 import App from './App';
 import Publish from './Publish';
-import Sell from './Sell';
 import NFTInfo from './NFTInfo';
-
 import SellSingle from './SellSingle';
 import BuySingle from './BuySingle';
 import Collections from './Collections';
@@ -15,7 +13,6 @@ const BasicRoute = () => (
         <Switch>
             <Route exact path="/" component={App}/>
             <Route exact path="/publish" component={Publish}/>
-            <Route exact path="/sell" component={Sell}/>
             <Route exact path="/sellSingle/:NFTId" component={SellSingle} />
             <Route exact path="/buySingle/:NFTId" component={BuySingle} />
             <Route exact path="/collections" component={Collections}/>
--- publish-token/src/TopBar.js
+++ publish-token/src/TopBar.js
@@ -105,9 +105,6 @@ class TopBar extends Component {
               <Button size="large" style={{ marginLeft: "1%" }} className={classes.btn} href='/#/publish'>
                 <b>发布</b>
               </Button>
-              <Button size="large" style={{ marginLeft: "1%" }} className={classes.btn} href='/#/sell'>
-                <b>售卖</b>
-              </Button>
 
 
               <Button size="large" style={{ marginLeft: "1%" }} className={classes.btn} href='/#/collections'>
